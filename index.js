2b30cccbd022c3ec253473fb43b801571aaf01c8
MINCHUL NA <nmc0307@gmail.com>
2025-06-01 01:11:50 +0900

    Add project files

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello API!' });
+});
+
+app.get('/callback', (req, res) => {
+  res.send('This is the callback endpoint!');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
